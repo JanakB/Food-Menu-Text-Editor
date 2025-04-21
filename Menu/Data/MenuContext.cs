65c2488907b1ac61a1dba14925d89f828fbf4186
JanakB <bhudathokijanak@gmail.com>
2025-04-21 12:06:51 +0545

    food menu

--- Menu/Data/MenuContext.cs
+++ Menu/Data/MenuContext.cs
@@ -9,7 +9,34 @@ namespace Menu.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new
+            {
+                di.DishId,
+                di.IngredientId
+            });
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id = 1, Name = "Margeritta", Price = 7.50, ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-GQ2TMDlb3dz_N_WOMWeOBHwz5YOMBElZA&s" },
+                new Dish { Id = 2, Name = "ghg", Price = 9, ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-GQ2TMDlb3dz_N_WOMWeOBHwz5YOMBElZA&s" }
+                );
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient { Id = 1, Name = "Tomato Sauce" },
+                new Ingredient { Id = 2, Name = "Mozrilla" }
+
+                );
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient { DishId=1, IngredientId=1},
+                new DishIngredient { DishId = 2, IngredientId = 2 }
+                );
+
+
             base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<DishIngredient> DishIngredients { get; set; }
     }
 }
--- Menu/Models/Dish.cs
+++ Menu/Models/Dish.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace Menu.Models
 {
     public class Dish
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
